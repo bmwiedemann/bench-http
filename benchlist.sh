928ff5e30c9f47a58aa2733792420eb2ea43201e
Bernhard M. Wiedemann <bwiedemann@suse.de>
2022-01-03 11:36:31 +0100

    Use a more common iso
    to be more likely to hit hot caches on the server

--- benchlist.sh
+++ benchlist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mirrors=$(curl http://download.opensuse.org/distribution/leap/15.3/iso/openSUSE-Leap-15.3-2-DVD-aarch64-Build24.5-Media.iso.mirrorlist|perl -ne 'm!^\s*<li><a href="([^"]+)"! && $1!~m!\.(meta4|metalink|torrent|magnet)$! && print "$1\n"')
+mirrors=$(curl http://download.opensuse.org/distribution/leap/15.3/iso/openSUSE-Leap-15.3-DVD-x86_64-Media.iso.mirrorlist|perl -ne 'm!^\s*<li><a href="([^"]+)"! && $1!~m!\.(meta4|metalink|torrent|magnet)$! && print "$1\n"')
 
 for m in $mirrors ; do
     timeout 2m ./bench-one.pl $m
