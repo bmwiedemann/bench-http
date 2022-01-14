740545c971522d6a9d26377521e3f8724671315c
Bernhard M. Wiedemann <bwiedemann@suse.de>
2022-01-14 19:03:13 +0100

    Force http for mirrors
    because authenticity is ensured via gpg in zypper
    and SSL handshakes take 1 extra RTT and encryption can slow things down, too

--- findbestmirror.pl
+++ findbestmirror.pl
@@ -8,6 +8,7 @@ while(<>) {
   my @a = split(":");
   my $m = { url=>"$a[0]:$a[1]", reply=>$a[2], RTT=>$a[3], d1=>$a[4], dataspeed=>$a[5], datasize=>$a[6]};
   $m->{url} =~ s{distribution/leap/..../iso/.*}{};
+  $m->{url} =~ s{^https}{http};
   next unless $m->{datasize} == 10230000;
   push(@mirrors, $m);
 }
