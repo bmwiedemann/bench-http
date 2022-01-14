92b8a116076babc277455bc9e2c91a662fe871fe
Bernhard M. Wiedemann <bwiedemann@suse.de>
2022-01-14 17:32:47 +0100

    Apply best mirror to configs
    to make it easy to use

--- findbestmirror.pl
+++ findbestmirror.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
+our $debug = 1;
 
 my @mirrors=();
 while(<>) {
   my @a = split(":");
   my $m = { url=>"$a[0]:$a[1]", reply=>$a[2], RTT=>$a[3], d1=>$a[4], dataspeed=>$a[5], datasize=>$a[6]};
+  $m->{url} =~ s{distribution/leap/..../iso/.*}{};
   next unless $m->{datasize} == 10230000;
   push(@mirrors, $m);
 }
@@ -14,7 +16,29 @@ sub metric($) { $_[0]->{dataspeed} + 2*$_[0]->{d1} + 6*$_[0]->{RTT} }
 
 print "fastest mirrors were:\n";
 foreach(@mirrors[0..3]) {
-  my $url = $_->{url};
-  $url =~ s{distribution/leap/..../iso/.*}{};
-  print "RTT=$_->{RTT}s 10M=$_->{dataspeed}s $url\n";
+  print "RTT=$_->{RTT}s 10M=$_->{dataspeed}s $_->{url}\n";
+}
+my $besturl=$mirrors[0]->{url};
+
+print "apply best mirror\n";
+for my $repo (</etc/zypp/repos.d/*.repo>) {
+  open(my $fd, "<", $repo) or die "could not read $repo: $!";
+  my @lines = <$fd>;
+  close($fd);
+  my $changed = 0;
+  for(my $li=$#lines; $li>=0; $li--) {
+    my $l = $lines[$li];
+    if($l=~m{^baseurl=http.?://download.opensuse.org/(.*)}) {
+      my $newline = "baseurl=$besturl$1\n";
+      if($l !~ m{/repositories/} and $lines[$li-1] !~ /^baseurl=/) {
+        splice(@lines, $li, 0, $newline);
+        $changed = 1;
+      }
+    }
+  }
+  next unless $changed;
+  print "\n>$repo:\n", @lines if $debug;
+  open($fd, ">", $repo) or die "could not write $repo: $!";
+  print $fd @lines;
+  close $fd;
 }
